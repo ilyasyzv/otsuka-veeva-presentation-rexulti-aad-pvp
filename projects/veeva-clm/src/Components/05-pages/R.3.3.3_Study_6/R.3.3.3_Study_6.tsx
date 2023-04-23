08da441822b994a094e1484d614a9323c7aaaa76
andrii_nimchenko <andrii_nimchenko@epam.com>
2023-04-23 18:40:28 +0300

    DF-33600 | R.3.3.3_Study_6

--- projects/veeva-clm/src/Components/05-pages/R.3.3.3_Study_6/R.3.3.3_Study_6.tsx
+++ projects/veeva-clm/src/Components/05-pages/R.3.3.3_Study_6/R.3.3.3_Study_6.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Link } from '@/Components/03-organisms/Routing/Link/Link';
+import { Footer } from '@/Components/03-organisms/Footer/Footer';
+import RexultiLogo from '@/assets/rexulti-logo.png';
+import Header from '@atoms/Content/Header/Header';
+import Paragraph from '@atoms/Content/Paragraph/Paragraph';
 import styles from './R.3.3.3_Study_6.module.scss';
+import PageImage from './img/Image1.png';
 
-const PageContent: React.FC = () => {
-  return <div className={styles.page}>R.3.3.3_Study_6 page</div>;
+const PageContent = () => {
+  return (
+    <div className={styles.page}>
+      <Header custom={`${styles.h1} mb-10 mt-24`}>
+        REXULTI<sup>®</sup> (brexpiprazole): Change in Frequency
+        <br /> Across Subscales of Agitation Symptoms
+      </Header>
+      <Paragraph
+        custom={`mb-24 ${styles.pivotal_trials}`}
+        children='In the pivotal trials, the change in frequency across CMAI subscales was measured as a secondary endpoint'
+      />
+      <Header type='h3' custom={`${styles.h3} mb-14`}>
+        Mean change in CMAI subscales<sup>1</sup>
+      </Header>
+      <img className={styles.image_1} src={PageImage} alt='' width='852' />
+      <Footer
+        custom={styles.footer}
+        references={{
+          data: [
+            {
+              text: <span>Data on file. Otsuka.</span>,
+            },
+          ],
+        }}
+        safetyInfo={{
+          custom: `mt-6 ${styles.safety_info}`,
+          data: [
+            {
+              text: (
+                <span>
+                  Please see{' '}
+                  <Link to='R.6.0_ISI'>IMPORTANT SAFETY INFORMATION</Link> in
+                  this presentation.
+                </span>
+              ),
+            },
+            {
+              text: 'FOR USE WITH FORMULARY DECISION-MAKERS ONLY. DO NOT DUPLICATE OR DISTRIBUTE.',
+            },
+          ],
+        }}
+        image={<img src={RexultiLogo} alt='maintena-logo' width='140.2' />}
+      />
+    </div>
+  );
 };
 export default PageContent;
