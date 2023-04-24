587c3e0a1cf58ab0707eb6b763b039e7cc05ebd9
Nikolai Tatianenko <nikolai_tatianenko@epam.com>
2023-04-24 17:30:50 +0200

    DF-33595 | AAD Agitation in dementia

--- projects/veeva-clm/src/Components/05-pages/R.2.4_AAD_Overview/R.2.4_AAD_Overview.tsx
+++ projects/veeva-clm/src/Components/05-pages/R.2.4_AAD_Overview/R.2.4_AAD_Overview.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './R.2.4_AAD_Overview.module.scss';
-
+import Header from '@/Components/01-atoms/Content/Header/Header';
+import { Footer } from '@/Components/03-organisms/Footer/Footer';
 const PageContent: React.FC = () => {
   return <div className={styles.page}>R.2.4_AAD_Overview page</div>;
 };
