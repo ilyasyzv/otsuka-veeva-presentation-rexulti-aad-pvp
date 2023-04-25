8aa04bdfc25dca1ce9d6670bebd9c11bf19fbeb6
Iaroslav <Iaroslav_Khomyn@epam.com>
2023-04-25 21:49:39 +0300

    Resolve conflicts.

--- projects/veeva-clm/src/Components/05-pages/R.3.5_Study_6&7_safety/R.3.5_Study_6&7_safety.tsx
+++ projects/veeva-clm/src/Components/05-pages/R.3.5_Study_6&7_safety/R.3.5_Study_6&7_safety.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import Header from '@atoms/Content/Header/Header';
 import List from '@atoms/Content/List/List';
+import Footer from '@organisms/Footer/Footer';
+import { Link } from '@organisms/Routing';
 
 import RexultiTable from './img/rexulti_table.png';
+import RexultiLogo from '@/assets/rexulti-logo.png';
 
 import styles from './R.3.5_Study_6&7_safety.module.scss';
-import {Link} from "@organisms/Routing";
-import RexultiLogo from "@/assets/rexulti-logo.png";
-import Footer from "@organisms/Footer/Footer";
-
 
 const PageContent: React.FC = () => {
   return (
     <div className={styles.page}>
       <Header custom={`${styles.h1} mt-29 mb-13`}>
-        REXULTI<sup className={'big'}>®</sup> (brexpiprazole) Safety Profile vs Placebo in AAD
+        REXULTI<sup className={'big'}>®</sup> (brexpiprazole) Safety Profile vs
+        Placebo in AAD
       </Header>
-      <Header  type={'h3'} custom={`mt-10 mb-13 pr-40`}>
-        Adverse reactions (ARs) in ≥2% of patients treated with REXULTI
-        and greater than placebo from two 12-week pivotal trials across all doses
+      <Header type={'h3'} custom={`mt-10 mb-13 pr-40`}>
+        Adverse reactions (ARs) in ≥2% of patients treated with REXULTI and
+        greater than placebo from two 12-week pivotal trials across all doses
       </Header>
 
       <img src={RexultiTable} alt='' width='815' />
@@ -36,12 +36,21 @@ const PageContent: React.FC = () => {
               custom_style: 'mb-1',
               text: (
                 <>
-                  <p><strong>Falls:</strong> Incidence of <strong>falls and dizziness</strong> in
-                  patients treated with REXULTI was <strong>similar</strong> compared to patients
-                    treated with placebo</p>
-                  <p className={styles.list_subtext}><span> - </span> <span>Incidence of falls pooled from trials of
-                  REXULTI (0.5 mg/day or 1 mg/day fixed, 2 mg/day or 3 mg/day fixed, or
-                    0.5-2 mg/ day&nbsp;flexible) vs placebo: 1.7% vs 2.6%<sup>1</sup></span></p>
+                  <p>
+                    <strong>Falls:</strong> Incidence of{' '}
+                    <strong>falls and dizziness</strong> in patients treated
+                    with REXULTI was <strong>similar</strong> compared to
+                    patients treated with placebo
+                  </p>
+                  <p className={styles.list_subtext}>
+                    <span> - </span>{' '}
+                    <span>
+                      Incidence of falls pooled from trials of REXULTI (0.5
+                      mg/day or 1 mg/day fixed, 2 mg/day or 3 mg/day fixed, or
+                      0.5-2 mg/ day&nbsp;flexible) vs placebo: 1.7% vs 2.6%
+                      <sup>1</sup>
+                    </span>
+                  </p>
                 </>
               ),
             },
@@ -49,9 +58,10 @@ const PageContent: React.FC = () => {
               custom_style: 'mb-1',
               text: (
                 <>
-                <strong>Cognitive Decline:</strong> No evidence of worsening or rapid cognitive decline
-                  based on a Mini-Mental State Examination (MMSE) score&nbsp;in comparison to
-                  placebo was observed
+                  <strong>Cognitive Decline:</strong> No evidence of worsening
+                  or rapid cognitive decline based on a Mini-Mental State
+                  Examination (MMSE) score&nbsp;in comparison to placebo was
+                  observed
                 </>
               ),
             },
@@ -59,9 +69,17 @@ const PageContent: React.FC = () => {
               custom_style: 'mb-1',
               text: (
                 <>
-                  <p>No QTc interval prolongation at 4x maximum recommended human dose (MRHD)</p>
-                  <p className={`${styles.list_subtext} mt-0`}><span> - </span> <span>At a dose 4 times the MRHD, REXULTI does not
-                    prolong QTc interval to any clinically relevant extent</span></p>
+                  <p>
+                    No QTc interval prolongation at 4x maximum recommended human
+                    dose (MRHD)
+                  </p>
+                  <p className={`${styles.list_subtext} mt-0`}>
+                    <span> - </span>{' '}
+                    <span>
+                      At a dose 4 times the MRHD, REXULTI does not prolong QTc
+                      interval to any clinically relevant extent
+                    </span>
+                  </p>
                 </>
               ),
             },
@@ -69,10 +87,22 @@ const PageContent: React.FC = () => {
               custom_style: 'mb-1',
               text: (
                 <>
-                  <p>A total of 5.6% (28/503) of patients treated with REXULTI and 4.8% (12/251) of patients
-                  treated with placebo discontinued due&nbsp;to&nbsp;ARs</p>
-                  <p className={styles.list_subtext}><span> - </span> <span>Approximately <strong>95%</strong> of patients <strong>remained on treatment without
-                    discontinuation</strong> due to adverse reactions, whether treated&nbsp;with&nbsp;REXULTI or placebo</span></p>
+                  <p>
+                    A total of 5.6% (28/503) of patients treated with REXULTI
+                    and 4.8% (12/251) of patients treated with placebo
+                    discontinued due&nbsp;to&nbsp;ARs
+                  </p>
+                  <p className={styles.list_subtext}>
+                    <span> - </span>{' '}
+                    <span>
+                      Approximately <strong>95%</strong> of patients{' '}
+                      <strong>
+                        remained on treatment without discontinuation
+                      </strong>{' '}
+                      due to adverse reactions, whether
+                      treated&nbsp;with&nbsp;REXULTI or placebo
+                    </span>
+                  </p>
                 </>
               ),
             },
