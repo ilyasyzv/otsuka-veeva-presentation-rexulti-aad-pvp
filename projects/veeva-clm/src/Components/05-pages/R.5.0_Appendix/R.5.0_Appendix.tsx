f3c4929db3696332f62eaed85007e6fe3012125f
Uladzislau Trehubau <Uladzislau_Trehubau@epam.com>
2023-04-25 15:39:55 +0200

    DF-33479 | R.5.0_Appendix

--- projects/veeva-clm/src/Components/05-pages/R.5.0_Appendix/R.5.0_Appendix.tsx
+++ projects/veeva-clm/src/Components/05-pages/R.5.0_Appendix/R.5.0_Appendix.tsx
@@ -1,7 +1,88 @@
 import React from 'react';
+
+import Tabs from '@organisms/Tabs/Tabs';
+import Tab from '@organisms/Tabs/Tab';
+import Header from '@atoms/Content/Header/Header';
+import Footer from '@organisms/Footer/Footer';
+import { Link } from '@organisms/Routing';
+
+import RexultiLogo from '@/assets/rexulti-logo.png';
+import imgCMAIOverview from './img/cmai_overview.png';
+import imgCMAIItems from './img/cmai_items.png';
+
 import styles from './R.5.0_Appendix.module.scss';
 
 const PageContent: React.FC = () => {
-  return <div className={styles.page}>R.5.0_Appendix page</div>;
+  return (
+    <>
+      <div className={styles.page}>
+        <Header type='h1' custom={styles.title}>
+          CMAI Details
+        </Header>
+        <Tabs translateTabsX={37}>
+          <Tab title='CMAI Overview'>
+            <img src={imgCMAIOverview} alt='CMAI Overview' />
+          </Tab>
+          <Tab title='CMAI Items'>
+            <img src={imgCMAIItems} alt='CMAI Items' />
+          </Tab>
+        </Tabs>
+      </div>
+
+      <Footer
+        custom={styles.footer}
+        footnotes={{
+          data: [
+            {
+              innerText:
+                'AAD, agitation associated with Alzheimer’s dementia; CMAI, Cohen-Mansfield Agitation Inventory; IPA, International Psychogeriatric Association.',
+            },
+          ],
+        }}
+        references={{
+          custom: 'mt-6',
+          data: [
+            {
+              text: (
+                <span>
+                  Cohen-Mansfield, J. (1991). Instruction Manual for the
+                  Cohen-Mansfield Agitation Inventory (CMAI). Rockville, MD:
+                  Research Institute of the Hebrew Home of Greater Washington.
+                  Available at: https://uiowa2.instructure.com › files ›
+                  download. Accessed March 17, 2023.
+                </span>
+              ),
+            },
+            {
+              text: (
+                <span>
+                  Grossberg GT, et al. <strong>Am J Geriatr Psychiatry.</strong>{' '}
+                  2020;28(4):383–400.
+                </span>
+              ),
+            },
+          ],
+        }}
+        safetyInfo={{
+          custom: `mt-6 ${styles.safety_info}`,
+          data: [
+            {
+              text: 'FOR USE WITH FORMULARY DECISION-MAKERS ONLY. DO NOT DUPLICATE OR DISTRIBUTE.',
+            },
+          ],
+        }}
+        image={
+          <div className={styles.footerLogo}>
+            <img
+              className={styles.footerLogoImg}
+              src={RexultiLogo}
+              alt='maintena-logo'
+              width='140.2'
+            />
+          </div>
+        }
+      />
+    </>
+  );
 };
 export default PageContent;
