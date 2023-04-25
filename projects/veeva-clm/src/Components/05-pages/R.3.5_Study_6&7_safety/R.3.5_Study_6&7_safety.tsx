ebd47881bac09ab228f1d9c7fe7e0a3372491a1a
Iaroslav <Iaroslav_Khomyn@epam.com>
2023-04-25 21:39:44 +0300

    DF-33475 | Study_6&7_safety page.

--- projects/veeva-clm/src/Components/05-pages/R.3.5_Study_6&7_safety/R.3.5_Study_6&7_safety.tsx
+++ projects/veeva-clm/src/Components/05-pages/R.3.5_Study_6&7_safety/R.3.5_Study_6&7_safety.tsx
@@ -1,7 +1,124 @@
 import React from 'react';
+import Header from '@atoms/Content/Header/Header';
+import List from '@atoms/Content/List/List';
+
+import RexultiTable from './img/rexulti_table.png';
+
 import styles from './R.3.5_Study_6&7_safety.module.scss';
+import {Link} from "@organisms/Routing";
+import RexultiLogo from "@/assets/rexulti-logo.png";
+import Footer from "@organisms/Footer/Footer";
+
 
 const PageContent: React.FC = () => {
-  return <div className={styles.page}>R.3.5_Study_6&7_safety page</div>;
+  return (
+    <div className={styles.page}>
+      <Header custom={`${styles.h1} mt-29 mb-13`}>
+        REXULTI<sup className={'big'}>®</sup> (brexpiprazole) Safety Profile vs Placebo in AAD
+      </Header>
+      <Header  type={'h3'} custom={`mt-10 mb-13 pr-40`}>
+        Adverse reactions (ARs) in ≥2% of patients treated with REXULTI
+        and greater than placebo from two 12-week pivotal trials across all doses
+      </Header>
+
+      <img src={RexultiTable} alt='' width='815' />
+      <List
+        content={{
+          custom: `${styles.list} mt-2`,
+          type: 'circle',
+          marker_color: 'green-300' /*optional*/,
+          data: [
+            {
+              custom_style: 'mb-1',
+              text: 'In clinical studies of 641 geriatric patients aged ≥65 years who received REXULTI for the treatment of AAD: ',
+            },
+            {
+              custom_style: 'mb-1',
+              text: (
+                <>
+                  <p><strong>Falls:</strong> Incidence of <strong>falls and dizziness</strong> in
+                  patients treated with REXULTI was <strong>similar</strong> compared to patients
+                    treated with placebo</p>
+                  <p className={styles.list_subtext}><span> - </span> <span>Incidence of falls pooled from trials of
+                  REXULTI (0.5 mg/day or 1 mg/day fixed, 2 mg/day or 3 mg/day fixed, or
+                    0.5-2 mg/ day&nbsp;flexible) vs placebo: 1.7% vs 2.6%<sup>1</sup></span></p>
+                </>
+              ),
+            },
+            {
+              custom_style: 'mb-1',
+              text: (
+                <>
+                <strong>Cognitive Decline:</strong> No evidence of worsening or rapid cognitive decline
+                  based on a Mini-Mental State Examination (MMSE) score&nbsp;in comparison to
+                  placebo was observed
+                </>
+              ),
+            },
+            {
+              custom_style: 'mb-1',
+              text: (
+                <>
+                  <p>No QTc interval prolongation at 4x maximum recommended human dose (MRHD)</p>
+                  <p className={`${styles.list_subtext} mt-0`}><span> - </span> <span>At a dose 4 times the MRHD, REXULTI does not
+                    prolong QTc interval to any clinically relevant extent</span></p>
+                </>
+              ),
+            },
+            {
+              custom_style: 'mb-1',
+              text: (
+                <>
+                  <p>A total of 5.6% (28/503) of patients treated with REXULTI and 4.8% (12/251) of patients
+                  treated with placebo discontinued due&nbsp;to&nbsp;ARs</p>
+                  <p className={styles.list_subtext}><span> - </span> <span>Approximately <strong>95%</strong> of patients <strong>remained on treatment without
+                    discontinuation</strong> due to adverse reactions, whether treated&nbsp;with&nbsp;REXULTI or placebo</span></p>
+                </>
+              ),
+            },
+          ],
+        }}
+      />
+      <Footer
+        custom={styles.footer}
+        footnotes={{
+          custom: 'mb-2',
+          data: [
+            {
+              innerText:
+                'AAD, agitation associated with Alzheimer’s dementia; QTc, corrected QT interval.',
+            },
+          ],
+        }}
+        references={{
+          data: [
+            {
+              text: 'Data on file. Otsuka.',
+            },
+          ],
+        }}
+        safetyInfo={{
+          custom: `mt-8 ${styles.safety_info}`,
+          data: [
+            {
+              text: (
+                <span className={'highlighted'}>
+                  Please see{' '}
+                  <Link to='R.6.0_ISI' custom={'green-200'}>
+                    IMPORTANT SAFETY INFORMATION
+                  </Link>{' '}
+                  in this presentation.
+                </span>
+              ),
+            },
+            {
+              text: 'FOR USE WITH FORMULARY DECISION-MAKERS ONLY. DO NOT DUPLICATE OR DISTRIBUTE.',
+            },
+          ],
+        }}
+        image={<img src={RexultiLogo} alt='maintena-logo' width='154.2' />}
+      />
+    </div>
+  );
 };
 export default PageContent;
