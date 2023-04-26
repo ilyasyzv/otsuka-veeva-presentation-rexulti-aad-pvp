3bbedbc0c26c815a8b2b6838591769faf635afa0
Uladzislau Trehubau <Uladzislau_Trehubau@epam.com>
2023-04-26 12:42:59 +0200

    DF-33479 | R.5.0_Appendix | Responsive tab size

--- projects/veeva-clm/src/Components/03-organisms/Tabs/subcomponents/CarretGroup.tsx
+++ projects/veeva-clm/src/Components/03-organisms/Tabs/subcomponents/CarretGroup.tsx
@@ -7,7 +7,9 @@ export type CarretGroupProps = {
   activeIndex: number;
   animationSpeed?: number;
   children: React.ReactNode;
+  childrenCount: number;
   translateTabsX?: number;
+  tabWidth?: number;
 };
 
 export const CarretGroup: React.FC<CarretGroupProps> = ({
@@ -15,19 +17,23 @@ export const CarretGroup: React.FC<CarretGroupProps> = ({
   activeIndex,
   animationSpeed = 0.5,
   children,
+  childrenCount,
   translateTabsX,
+  tabWidth,
 }) => {
-  const leftIndent = carretSize * activeIndex;
+  const carretWidth = tabWidth || carretSize;
+  const leftIndent = carretWidth * activeIndex;
 
   return (
     <div
       className={style.tabCarretWrapper}
       style={{
+        width: tabWidth && `${tabWidth * childrenCount}px`,
         transform: translateTabsX ? `translateX(${translateTabsX}px)` : 'none',
       }}
     >
       <Carret
-        carretSize={carretSize}
+        carretSize={carretWidth}
         leftIndent={leftIndent}
         animationSpeed={animationSpeed}
         children={children}
